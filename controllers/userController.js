db44e7501ddaca754b5d8bf6d8a75697750a69ba
Friis Kraas <friis.kraas@gmx.de>
2023-06-20 11:21:03 +0200

    added user update logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // async function createUser(req, res) {
 //   try {
@@ -14,10 +15,26 @@ const User = require("../models/User");
 async function updateUser(req, res) {
   try {
     const { user_id } = req.params;
-    const newUserData = req.body;
-    const updatedUser = await User.findByIdAndUpdate(user_id, newUserData, {
-      new: true,
-    });
+    const { email, password } = req.body;
+    let updatedUser;
+    if (!password && email) {
+      updatedUser = await User.findByIdAndUpdate(
+        user_id,
+        { email },
+        {
+          new: true,
+        }
+      );
+    } else if (password && !email) {
+      const hash = await bcrypt.hash(password, 8);
+      updatedUser = await User.findByIdAndUpdate(
+        user_id,
+        { password: hash },
+        {
+          new: true,
+        }
+      );
+    } else throw new Error("Specify either email or password");
     res.status(200).json(updatedUser);
   } catch (err) {
     console.log(err);
